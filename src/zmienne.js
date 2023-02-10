7c7bea50f5f001c0e7d34ffec0258e5b63743d8a
Emil Juchnikowski <emil@juchnikowski.com>
2023-02-10 12:08:45 +0100

    zmienne

---
+++ src/zmienne.js
@@ -0,0 +1,11 @@
+if (true) {
+    var zminna1 = 2;
+    zminna1 = 3;
+}
+if (true) {
+    var zmienna2 = 4;
+}
+zmienna2 = 5;
+if (true) {
+    var zmienna3 = 6;
+}
---
+++ src/zmienne.ts
@@ -0,0 +1,14 @@
+if (true) {
+    let zminna1: number = 2;
+    zminna1 = 3;
+}
+
+if (true) {
+    var zmienna2: number = 4;
+}
+
+zmienna2 = 5;
+
+if (true) {
+    const zmienna3: number = 6;
+}
