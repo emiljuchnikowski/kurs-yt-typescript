5c96092c4ccfa46dd9b8a8ab20488f52eaf2ce80
Emil Juchnikowski <emil@juchnikowski.com>
2023-03-24 11:31:59 +0100

    warunki

---
+++ src/warunki.ts
@@ -0,0 +1,7 @@
+const zmiennaDoWaunku : number = 3;
+
+if (zmiennaDoWaunku > 2) {
+    renderujTekst("warunki", "Zmienna do warunku jest wieksza od 2");
+} else {
+    renderujTekst("warunki", "Zmienna do warunku nie jest wieksza od 2");
+}
