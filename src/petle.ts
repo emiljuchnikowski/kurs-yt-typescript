93a306dc678f4ad6bd9a3b2a39ace0d922069602
Emil Juchnikowski <emil@juchnikowski.com>
2023-03-31 11:29:44 +0200

    petle

---
+++ src/petle.ts
@@ -0,0 +1,5 @@
+for(let index = 0; index < 3; index++) {
+    const idElmentu = "petla" + index;
+    const textDoElementu = "Uzupelniony elemenet z index: " + index;
+    renderujTekst(idElmentu, textDoElementu);
+}
