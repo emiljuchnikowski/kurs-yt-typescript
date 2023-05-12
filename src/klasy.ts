432084dad512396141aceb6daa531e0634b82781
Emil Juchnikowski <emil@juchnikowski.com>
2023-05-12 09:33:59 +0200

    interfejsy

--- src/klasy.ts
+++ src/klasy.ts
@@ -1,4 +1,13 @@
-class Samochod {
+interface Pojazd {
+    jedz(km: number): void;
+    opiszStan(): string;
+}
+
+interface Samochod extends Pojazd {
+    licznik: number;
+}
+
+class Bmw implements Samochod {
     licznik: number = 0;
 
     jedz(km: number): void {
@@ -6,11 +15,23 @@ class Samochod {
     }
 
     opiszStan(): string {
-        return "Przebieg auta: " + this.licznik + " km";
+        return "Przebieg auta (bmw): " + this.licznik + " km";
     }
 }
 
-const samochod1: Samochod = new Samochod();
-samochod1.jedz(100);
+class Opel implements Samochod {
+    licznik: number = 0;
 
-renderujTekst("stanLicznikaAuta", samochod1.opiszStan());
+    jedz(km: number): void {
+        this.licznik += km;
+    }
+
+    opiszStan(): string {
+        return "Przebieg auta (opel): " + this.licznik + " km";
+    }
+}
+
+let samochod1: Samochod = new Bmw();
+samochod1.jedz(100);
+samochod1 = new Opel();
+samochod1.jedz(100);
