73d8adf5c49e482dacfdb1cb13d572f54e55dab6
Emil Juchnikowski <emil@juchnikowski.com>
2023-06-09 12:23:21 +0200

    wlasciwosci

--- src/klasy.ts
+++ src/klasy.ts
@@ -5,6 +5,22 @@ interface Pojazd {
 
 abstract class Samochod implements Pojazd {
     private licznik: number = 0;
+    private pasazerowieWSamochodzie = 0;
+
+    protected maksymalnaIloscPasazerow = 5;
+
+    get pasazerowie(): number {
+        return this.pasazerowieWSamochodzie;
+    }
+
+    set pasazerowie(ilosc: number) {
+        if (ilosc < 0) throw new Error("Ilosci pasazerow musi byc wieksza od zera");
+        if (ilosc > this.maksymalnaIloscPasazerow) { 
+            throw new Error("Ilosci pasazerow musi byc wieksza mniejsza od: " + (this.maksymalnaIloscPasazerow + 1));
+        }
+
+        this.pasazerowieWSamochodzie = ilosc;
+    }
 
     jedz(km: number): void {
         this.licznik += km;
@@ -22,6 +38,11 @@ abstract class Samochod implements Pojazd {
 }
 
 class Bmw extends Samochod {
+    constructor() {
+        super();
+        this.maksymalnaIloscPasazerow = 4;
+    }
+
     opiszStan(): string {
         return this.zwrocSformatowanyStan("bmw");
     }
@@ -48,6 +69,8 @@ class Opel extends Samochod {
 
 let samochod1: Samochod = new Bmw();
 samochod1.jedz(100);
+samochod1.pasazerowie = 3;
+const pasazerowie = samochod1.pasazerowie;
 samochod1 = new Opel();
 samochod1.jedz(100);
 
